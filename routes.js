d7c459a0307ac34c5737e822c4cbe8902cd617ac
rahulguha <rahul.guha@gmail.com>
2015-02-17 08:26:06 +0530

    1

--- routes.js
+++ routes.js
@@ -10,13 +10,13 @@ var api = require('./api.js');
 
 function routes() {
 
-	this.insert_audit_request = function (req, res) {
-    	api.insert_audit_request(req, res);
-	};
-
-	this.get_audit_request_by_app_id = function (req, res) {
-    	api.get_audit_request_by_app_id(req, res);
-	};
+//	this.insert_audit_request = function (req, res) {
+//    	api.insert_audit_request(req, res);
+//	};
+//
+//	this.get_audit_request_by_app_id = function (req, res) {
+//    	api.get_audit_request_by_app_id(req, res);
+//	};
 
 	this.auth = function (req, res,callback) {
     	api.autheticate(req,res,  function (r){
